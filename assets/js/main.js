e2139b8cf3e67b93204172457339accf8e49a0d0
Pedro Henrique <phfs28@gmail.com>
2021-11-22 13:30:02 -0300

    Created the header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink =  document.querySelectorAll('.nav__link');
 
+const linkAction = () => {
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(link => link.addEventListener('click', linkAction));
 
 /*=============== HOME SWIPER ===============*/
 
