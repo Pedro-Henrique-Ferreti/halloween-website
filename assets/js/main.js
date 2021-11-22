af105729764ed1f695cadcd52e449d0c32c5cba5
Pedro Henrique <phfs28@gmail.com>
2021-11-22 14:54:11 -0300

    Created Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,14 @@ const linkAction = () => {
 navLink.forEach(link => link.addEventListener('click', linkAction));
 
 /*=============== HOME SWIPER ===============*/
-
+let homeSwiper = new Swiper('.home-swiper', {
+  spaceBetween: 30,
+  loop: 'true',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
